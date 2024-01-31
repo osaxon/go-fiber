8edf2ecb8c2a3860acc2eca56ac2f28b21d46c0e
Oliver Saxon <oliverrsaxon@gmail.com>
2024-01-31 20:03:48 +0000

    adds working api route

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"fiber.osaxon/router"
 	"github.com/gofiber/fiber/v2"
 	// "github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -16,6 +17,8 @@ func main() {
 		AppName:       "GoFiber",
 	})
 	// app.Use(cors.New())
+
+	router.SetupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 }
